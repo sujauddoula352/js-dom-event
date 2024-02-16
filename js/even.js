28d36371ed407c13adc10cf5762a25371c208937
SH_SOHEL <sujauddoulasohel352@gmail.com>
2024-02-16 19:16:02 +0600

    event onclicl type

--- js/even.js
+++ js/even.js
@@ -1,1 +1,18 @@
 console.log('the is a separate JS file');
+// option 2
+function makeRed(){
+    document.body.style.backgroundColor = 'red'
+}
+//option 3
+const makeBlueButton = document.getElementById('make-blue');
+makeBlueButton.onclick = makeBlue;
+
+function makeBlue(){
+    document.body.style.backgroundColor = 'blue'
+}
+
+//option 3 other
+const makePinkButton = document.getElementById('make-pink');
+ makePinkButton.onclick = function makepink(){
+    document.body.style.backgroundColor = 'pink'
+ }
